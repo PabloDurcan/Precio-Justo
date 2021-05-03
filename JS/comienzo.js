ed8bba8407c0ac5284276ecf863507d77fce58fd
Pablo <pablodurcan@gmail.com>
2021-05-03 11:18:38 +0200

    Primero

---
+++ JS/comienzo.js
@@ -0,0 +1,9 @@
+function comenzarJuego(){
+    document.getElementById("P-inicio").style.display = "none";
+    document.getElementById("P-comienzo").style.display = "flex";
+}
+
+function prueba() { 
+    document.getElementById("wrapper").style.display = "none";
+    document.getElementById("producto").style.display = "flex";
+ }
